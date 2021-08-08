297a22a5ab48087847a3a34184fc3df0498ab6cd
Хорошев Алексей Алексеевич <alexwerber42@gmail.com>
2021-08-08 14:57:03 +0700

    feat: add joke repository

--- app/src/main/java/xaa/werber/chuck_norris_joke/data/repotisory/JokeRepository.kt
+++ app/src/main/java/xaa/werber/chuck_norris_joke/data/repotisory/JokeRepository.kt
@@ -1,4 +1,31 @@
 package xaa.werber.chuck_norris_joke.data.repotisory
 
-class JokeRepository {
+import androidx.lifecycle.MutableLiveData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import xaa.werber.chuck_norris_joke.data.entity.Joke
+import xaa.werber.chuck_norris_joke.data.remote.ApiService
+import xaa.werber.chuck_norris_joke.data.remote.models.MainResponse
+
+class JokeRepository(private val apiService: ApiService) {
+    private var jokeList: MutableLiveData<List<Joke>> = MutableLiveData()
+
+    fun getData(): MutableLiveData<List<Joke>> = jokeList
+
+    fun loadJokeFromNetwork(number: Int) {
+        apiService
+                .getJoke(number)
+                .enqueue(object: Callback<MainResponse> {
+                    override fun onResponse(call: Call<MainResponse>, response: Response<MainResponse>) {
+                        if (response.isSuccessful) {
+                            response.body()?.let { jokeList.value = it.listOfJokes }
+                        }
+                    }
+
+                    override fun onFailure(call: Call<MainResponse>, t: Throwable) {
+
+                    }
+                })
+    }
 }
