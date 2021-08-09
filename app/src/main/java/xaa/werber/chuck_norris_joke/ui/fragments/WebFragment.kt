98de0534cc52dc9809de89e24084072fc8d446d0
Хорошев Алексей Алексеевич <alexwerber42@gmail.com>
2021-08-09 19:13:29 +0700

    feat: add web view fragment

--- app/src/main/java/xaa/werber/chuck_norris_joke/ui/fragments/WebFragment.kt
+++ app/src/main/java/xaa/werber/chuck_norris_joke/ui/fragments/WebFragment.kt
@@ -1,13 +1,40 @@
 package xaa.werber.chuck_norris_joke.ui.fragments
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebView
 import androidx.fragment.app.Fragment
 import dagger.android.support.AndroidSupportInjection
+import kotlinx.android.synthetic.main.fragment_web.*
 import xaa.werber.chuck_norris_joke.R
+import xaa.werber.chuck_norris_joke.utils.ApiConstants.WEB_URL
 
-class WebFragment: Fragment(R.layout.fragment_web) {
+class WebFragment: Fragment() {
+    private lateinit var webView: WebView
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        webView.saveState(outState)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        savedInstanceState?.let { webView.restoreState(it) }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidSupportInjection.inject(this)
         super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_web, container, false)
+
+        webView = view.findViewById(R.id.web_view)
+        webView.loadUrl(WEB_URL)
+
+        return view
     }
 }
--- app/src/main/java/xaa/werber/chuck_norris_joke/utils/ApiConstants.kt
+++ app/src/main/java/xaa/werber/chuck_norris_joke/utils/ApiConstants.kt
@@ -2,5 +2,6 @@ package xaa.werber.chuck_norris_joke.utils
 
 object ApiConstants {
     const val BASE_URL = "http://api.icndb.com/jokes/"
+    const val WEB_URL = "http://www.icndb.com/api/"
     const val READ_TIME = 5000L
 }
