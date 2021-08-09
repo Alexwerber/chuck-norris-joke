38874bf51fcb103f0e5ddd3348c562ad659906f8
Хорошев Алексей Алексеевич <alexwerber42@gmail.com>
2021-08-09 11:47:35 +0700

    feat: add fragment builder module

--- app/src/main/java/xaa/werber/chuck_norris_joke/di/builders/FragmentsBuilderModule.kt
+++ app/src/main/java/xaa/werber/chuck_norris_joke/di/builders/FragmentsBuilderModule.kt
@@ -1,8 +1,15 @@
 package xaa.werber.chuck_norris_joke.di.builders
 
 import dagger.Module
+import dagger.android.ContributesAndroidInjector
+import xaa.werber.chuck_norris_joke.ui.fragments.JokeListFragment
+import xaa.werber.chuck_norris_joke.ui.fragments.WebFragment
 
 @Module
 abstract class FragmentsBuilderModule {
+    @ContributesAndroidInjector
+    abstract fun contributeJokeListFragment(): JokeListFragment
 
+    @ContributesAndroidInjector
+    abstract fun contributeWebFragment(): WebFragment
 }
